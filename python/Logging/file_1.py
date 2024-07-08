a88a83554476712f4642832d502647db10823918
Tommy Vo <tommyvo2011@live.com>
2024-07-08 15:30:08 -0400

    Changes to the logging files

--- python/Logging/file_1.py
+++ python/Logging/file_1.py
@@ -4,15 +4,16 @@ logfile = "log_test.txt"
 log = open(logfile, "w")
 root_path = "C:\Test_Folder"
 
-
 def log_txt():
     os.chdir(root_path)
     log    
 
+
 list_of_dir = []
 
+os.chdir(root_path)
 
-# Grabs the subdirectories' name
+# Grabs the directories' names
 for x in os.listdir():
     list_of_dir.append(x)
 print(list_of_dir)
@@ -26,14 +27,15 @@ for i in list_of_dir:
         print(os.getcwd())
         #Creates a list of the files names and append them to an array
         for j in os.listdir():
-            list_of_files.append(j)
-            os.chdir(j)
-            if(os.)
             try:
-                log
-            except IOError:
-                log_txt()
-                log.write(i, "\t", j)
+                with open(j, "a+"):
+                    j.close()
+            except OSError:
+                os.chdir(root_path)
+                list_of_files.append(j)
+                with open("log_test.txt", "a+"):
+                    log.append(j + "\n")
+                    log.close()
     except NotADirectoryError:
         continue
           
