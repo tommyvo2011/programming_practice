c4d0e546ec4a27c7f557701cea2df8e1b915baf9
Tommy Vo <tvo39@student.wgu.edu>
2024-07-09 00:26:54 -0400

    Added subdirectory loop

--- python/Logging/file_1.py
+++ python/Logging/file_1.py
@@ -3,47 +3,63 @@ import pymupdf as pm
 
 logfile = "log_test.txt"
 log = open(logfile, "w")
-root_path = "F:\Programming\Test_Folder"
+root_path = "F:\Programming\Test_Folder_CDR"
 
 # Check for the number of pages within the document if
 # unable then it will create an exception error and add to log
 def check_pdf_corruption():
     try:
-        doc = pm.open(j)
+        doc = pm.open(k)
         num_pages = doc.page_count
         print(num_pages)
         doc.close()
     except:
-        print("Error File: " + j)
+        print("Error File: " + k)
         os.chdir(root_path)
-        list_of_files.append(j)
+        list_of_files.append(k)
         log = open("log_test.txt","a")
         log.write(i + "\t")
-        log.write(j + "\n")
+        log.write(j + "\t")
+        log.write(k + "\n")
         log.close()
         os.chdir(i)
 
 list_of_dir = []
-
 os.chdir(root_path)
 
 # Grabs the directories' names
-for x in os.listdir():
-    list_of_dir.append(x)
+for dir in os.listdir():
+    list_of_dir.append(dir)
 print(list_of_dir)
 
-#Grabs the files' name
+# Files through each Project Contract ID
 for i in list_of_dir:
+    list_of_subdir = []
     list_of_files = []
     try:
+        # Puts all the subdirectories inside of the Project Contract ID into an array
         os.chdir(i)
+        for subdir in os.listdir():
+             list_of_subdir.append(subdir)
+             print(subdir)
+
         print(os.getcwd())
-        #Creates a list of the files names and append them to an array
-        for j in os.listdir():
-            check_pdf_corruption()
+        # Checks List of Subdirectories
+        for j in list_of_subdir:
+            print(os.getcwd())
+            try:
+                os.chdir(j)
+                print(os.getcwd())
+                # Checks List of Files
+                for k in os.listdir():
+                    print(os.getcwd())
+                    check_pdf_corruption()
+           
+            except NotADirectoryError:
+                continue
+
     except NotADirectoryError:
-        continue
-          
+                continue
     print(list_of_files)
     os.chdir(root_path)
     print(os.getcwd())
