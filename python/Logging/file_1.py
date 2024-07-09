398b29e45ca6e5a01ca8c1d97a687a3d66ca5021
Tommy Vo <tvo39@student.wgu.edu>
2024-07-08 22:51:07 -0400

    Created a function to check for corruption

--- python/Logging/file_1.py
+++ python/Logging/file_1.py
@@ -1,26 +1,27 @@
 import os
-import fitz as fr
+import pymupdf as pm
 
 logfile = "log_test.txt"
 log = open(logfile, "w")
 root_path = "F:\Programming\Test_Folder"
 
-def log_txt():
-    os.chdir(root_path)
-    log    
-
+# Check for the number of pages within the document if
+# unable then it will create an exception error and add to log
 def check_pdf_corruption():
     try:
-        f = open(j, "r")
-        font_check = fr.CheckFont
+        doc = pm.open(j)
+        num_pages = doc.page_count
+        print(num_pages)
+        doc.close()
     except:
-        print("Error File: " +j)
+        print("Error File: " + j)
         os.chdir(root_path)
         list_of_files.append(j)
         log = open("log_test.txt","a")
         log.write(i + "\t")
         log.write(j + "\n")
         log.close()
+        os.chdir(i)
 
 list_of_dir = []
 
@@ -34,29 +35,15 @@ print(list_of_dir)
 #Grabs the files' name
 for i in list_of_dir:
     list_of_files = []
-    
     try:
         os.chdir(i)
         print(os.getcwd())
         #Creates a list of the files names and append them to an array
         for j in os.listdir():
-            try:
-                f = open(j, "r")
-                font_check = fr.get_text_length
-                print(font_check)
-                f.close()
-            except:
-                print("Error File: " +j)
-                os.chdir(root_path)
-                list_of_files.append(j)
-                log = open("log_test.txt","a")
-                log.write(i + "\t")
-                log.write(j + "\n")
-                log.close()
+            check_pdf_corruption()
     except NotADirectoryError:
         continue
           
     print(list_of_files)
-    #os.chdir("../")
     os.chdir(root_path)
     print(os.getcwd())
